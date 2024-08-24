4b5a9c5ffb4731086a2da52cb235f6fd87992d10
hskrasek <hskrasek@users.noreply.github.com>
2024-08-24 05:30:15 +0000

    Fix styling

--- src/ValinorServiceProvider.php
+++ src/ValinorServiceProvider.php
@@ -6,12 +6,7 @@ namespace HSkrasek\LaravelValinor;
 
 use CuyZ\Valinor\Mapper\TreeMapper;
 use CuyZ\Valinor\MapperBuilder;
-use HSkrasek\LaravelValinor\Contracts\ParsesWhenResolved;
-use HSkrasek\LaravelValinor\Http\FormRequest;
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Foundation\Http\FormRequest as IlluminateFormRequest;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Redirector;
 use Illuminate\Support\ServiceProvider;
 
 class ValinorServiceProvider extends ServiceProvider
